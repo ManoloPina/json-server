56b9dee3f8eeac537adf06fd2c0738bc0b753ebb
Manolo <manolobarbosapina@gmail.com>
2021-03-18 19:19:30 -0300

    Updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const faker = require('faker');
 module.exports = () => {
   const data = { batches: {content: [], message: "Ação Efetuada com Sucesso", typeResult: 1} };
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 20; i++) {
     data.batches.content.push({ 
       id: faker.random.uuid(), 
       companyName: faker.company.companyName(),

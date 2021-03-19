e7873b28a3e4ebb903c6f7dd297259c74acb7c68
Manolo <manolobarbosapina@gmail.com>
2021-03-19 12:28:38 -0300

    updated

---
+++ create-db.js
@@ -0,0 +1,45 @@
+const faker = require('faker');
+const fakerBr = require('faker-br');
+const fs = require('fs');
+const fileName = './db.json';
+const file = require(fileName);
+
+  const data = { batches: {content: [], message: "Ação Efetuada com Sucesso", typeResult: 1}, batch: {
+    content: {
+      id: faker.random.uuid(), 
+      companyName: faker.helpers.replaceSymbols("????"),
+      date: faker.date.recent(),
+      processedBillings: faker.random.number({min:0, max: 500}), 
+      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
+      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+      billings: [],
+    },
+    message: "Ação Efetuada com Sucesso",
+    typeResult: 1,
+  }  };
+
+  for (let i = 0; i < 20; i++) {
+    data.batches.content.push({ 
+      id: faker.random.uuid(), 
+      companyName: faker.helpers.replaceSymbols("????"),
+      date: faker.date.recent(),
+      processedBillings: faker.random.number({min:0, max: 500}), 
+      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
+      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+    })
+
+    data.batch.content.billings.push({
+      clientName: faker.helpers.replaceSymbols("????"),
+      document: faker.helpers.randomize([fakerBr.br.cnpj(), fakerBr.br.cpf()]),
+      contractCode: faker.helpers.replaceSymbols("????####"),
+      billingCode: faker.helpers.replaceSymbols("????####"),
+      rpsCode: faker.helpers.replaceSymbols("????####"),
+      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+    })
+  }
+
+  fs.writeFile(fileName, JSON.stringify(data), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('writing to ' + fileName);
+  });
--- server.js
+++ server.js
@@ -1,44 +1,10 @@
-const faker = require('faker');
-const fakerBr = require('faker-br');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
 
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const port = process.env.PORT || 4000;
+server.use(middlewares);
+server.use(router);
 
-server.listen(port, () => {
-  const data = { batches: {content: [], message: "Ação Efetuada com Sucesso", typeResult: 1}, batch: {
-    content: {
-      id: faker.random.uuid(), 
-      companyName: faker.helpers.replaceSymbols("????"),
-      date: faker.date.recent(),
-      processedBillings: faker.random.number({min:0, max: 500}), 
-      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
-      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
-      billings: [],
-    },
-    message: "Ação Efetuada com Sucesso",
-    typeResult: 1,
-  }  };
-
-  for (let i = 0; i < 10; i++) {
-    data.batches.content.push({ 
-      id: faker.random.uuid(), 
-      companyName: faker.helpers.replaceSymbols("????"),
-      date: faker.date.recent(),
-      processedBillings: faker.random.number({min:0, max: 500}), 
-      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
-      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
-    })
-
-    data.batch.content.billings.push({
-      clientName: faker.helpers.replaceSymbols("????"),
-      document: faker.helpers.randomize([fakerBr.br.cnpj(), fakerBr.br.cpf()]),
-      contractCode: faker.helpers.replaceSymbols("????####"),
-      billingCode: faker.helpers.replaceSymbols("????####"),
-      rpsCode: faker.helpers.replaceSymbols("????####"),
-      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
-    })
-  }
-  return data;
-})
+server.listen(port);

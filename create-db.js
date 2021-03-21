916932aac9102f416fd6095eb83d7ca41035787c
Manolo <manolobarbosapina@gmail.com>
2021-03-21 19:21:43 -0300

    updated endpoints

--- create-db.js
+++ create-db.js
@@ -1,46 +1,79 @@
-const faker = require('faker');
-const fakerBr = require('faker-br');
-const fs = require('fs');
-const fileName = './db.json';
+const faker = require("faker");
+const fakerBr = require("faker-br");
+const fs = require("fs");
+const fileName = "./db.json";
 const file = require(fileName);
 
-  const data = { batches: {content: [], message: "Ação Efetuada com Sucesso", typeResult: 1}, batch: {
+const data = {
+  batches: {
+    content: [],
+    message: "Ação Efetuada com Sucesso",
+    typeResult: 1,
+  },
+  batch: {
     content: {
-      id: faker.random.uuid(), 
+      id: faker.random.uuid(),
       companyName: faker.helpers.replaceSymbols("????"),
       date: faker.date.recent(),
-      processedBillings: faker.random.number({min:0, max: 500}), 
-      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
+      processedBillings: faker.random.number({ min: 0, max: 500 }),
+      totalRunningBillings: faker.random.number({ min: 500, max: 1000 }),
       batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
       billings: [],
-      canceledBillings: faker.random.number({min: 0, max: 500}),
+      canceledBillings: faker.random.number({ min: 0, max: 500 }),
     },
     message: "Ação Efetuada com Sucesso",
     typeResult: 1,
-  }  };
+  },
+  billingDetailed: {
+    id: faker.random.uuid(),
+    clientName: faker.helpers.replaceSymbols("????"),
+    document: faker.helpers.randomize([fakerBr.br.cnpj(), fakerBr.br.cpf()]),
+    contractCode: faker.helpers.replaceSymbols("????####"),
+    billingCode: faker.helpers.replaceSymbols("????####"),
+    rpsCode: faker.helpers.replaceSymbols("????####"),
+    billingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+    items: [],
+  },
+};
 
-  for (let i = 0; i < 20; i++) {
-    data.batches.content.push({ 
-      id: faker.random.uuid(), 
-      companyName: faker.helpers.replaceSymbols("????"),
-      date: faker.date.recent(),
-      processedBillings: faker.random.number({min:0, max: 500}), 
-      totalRunningBillings: faker.random.number({min: 500, max: 1000}),
-      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
-    })
-
-    data.batch.content.billings.push({
-      clientName: faker.helpers.replaceSymbols("????"),
-      document: faker.helpers.randomize([fakerBr.br.cnpj(), fakerBr.br.cpf()]),
-      contractCode: faker.helpers.replaceSymbols("????####"),
-      billingCode: faker.helpers.replaceSymbols("????####"),
-      rpsCode: faker.helpers.replaceSymbols("????####"),
-      batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
-    })
-  }
-
-  fs.writeFile(fileName, JSON.stringify(data), function writeJSON(err) {
-    if (err) return console.log(err);
-    console.log(JSON.stringify(file));
-    console.log('writing to ' + fileName);
+for (let i = 0; i < 20; i++) {
+  data.batches.content.push({
+    id: faker.random.uuid(),
+    companyName: faker.helpers.replaceSymbols("????"),
+    date: faker.date.recent(),
+    processedBillings: faker.random.number({ min: 0, max: 500 }),
+    totalRunningBillings: faker.random.number({ min: 500, max: 1000 }),
+    batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
   });
+
+  data.batch.content.billings.push({
+    clientName: faker.helpers.replaceSymbols("????"),
+    document: faker.helpers.randomize([fakerBr.br.cnpj(), fakerBr.br.cpf()]),
+    contractCode: faker.helpers.replaceSymbols("????####"),
+    billingCode: faker.helpers.replaceSymbols("????####"),
+    rpsCode: faker.helpers.replaceSymbols("????####"),
+    batchBillingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+  });
+}
+
+for (let i = 0; i < 5; i++) {
+  data.billingDetailed.items.push({
+    description: faker.helpers.randomize([
+      "Mensalidade de Pontos",
+      "Desconto",
+      "Aditivo",
+      "Cobrança de Excedente",
+      "Mão de Obra",
+      "Valor da Matriz",
+    ]),
+    referenceDate: faker.date.recent(),
+    billingType: faker.helpers.randomize(["Mensalidade", "Anual"]),
+    price: faker.commerce.price(500, 1000),
+  });
+}
+
+fs.writeFile(fileName, JSON.stringify(data), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log("writing to " + fileName);
+});
